165477ef9c051ad55d7e50686a1f8abe2606d3ec
Smoofington <caudill.matthew0127@gmail.com>
2022-09-09 01:35:20 -0500

    Initial commit

---
+++ src/main/java/com/findmyrecycling/fmrenterprise/FmRenterpriseApplication.java
@@ -0,0 +1,13 @@
+package com.findmyrecycling.fmrenterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmRenterpriseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmRenterpriseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/findmyrecycling/fmrenterprise/FmRenterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findmyrecycling.fmrenterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmRenterpriseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
