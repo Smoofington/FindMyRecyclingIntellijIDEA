1fe1a074b4d956324948cd5f634bb1461d00f896
samgehrlich <gehrlism@mail.uc.edu>
2022-09-13 20:53:26 -0400

    adding files

--- src/main/java/com/findmyrecycling/fmrenterprise/FmRenterpriseApplication.java
+++ src/main/java/com/findmyrecycling/fmrenterprise/FmRenterpriseApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FmRenterpriseApplication {
+public class FmRenterpriseApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(FmRenterpriseApplication.class, args);
