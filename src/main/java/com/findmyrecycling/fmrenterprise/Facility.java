5f9bcdc7802e99ec750141e783527d8aaaf381c9
matt-dupont <42855570+matt-dupont@users.noreply.github.com>
2022-09-21 15:16:53 -0400

    example class, please comment

---
+++ src/main/java/com/findmyrecycling/fmrenterprise/Facility.java
@@ -0,0 +1,55 @@
+package com.findmyrecycling.fmrenterprise;
+
+public class Facility {
+    private Long facilityId;
+    private Long materialId;
+    private String facilityName;
+    private String facilityPhotos;
+    private String facilityAddress;
+    private Decimal facilityCoordinates;
+
+    public Facility(Long facilityId, Long materialId, String facilityName, String facilityPhotos, String facilityAddress, Decimal facilityCoordinates) {
+        this.facilityId = facilityId;
+        this.materialId = materialId;
+        this.facilityName = facilityName;
+        this.facilityPhotos = facilityPhotos;
+        this.facilityAddress = facilityAddress;
+        this.facilityCoordinates = facilityCoordinates;
+    }
+
+    public Long getFacilityId() {
+        return facilityId;
+    }
+
+    public Long getMaterialId() {
+        return materialId;
+    }
+
+    public String getFacilityName() {
+        return facilityName;
+    }
+
+    public String getFacilityPhotos() {
+        return facilityPhotos;
+    }
+
+    public String getFacilityAddress() {
+        return facilityAddress;
+    }
+
+    public Decimal getFacilityCoordinates() {
+        return facilityCoordinates;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Facility{" +
+                "facilityId=" + facilityId +
+                ", materialId=" + materialId +
+                ", facilityName='" + facilityName + '\'' +
+                ", facilityPhotos='" + facilityPhotos + '\'' +
+                ", facilityAddress='" + facilityAddress + '\'' +
+                ", facilityCoordinates=" + facilityCoordinates +
+                '}';
+    }
+}
