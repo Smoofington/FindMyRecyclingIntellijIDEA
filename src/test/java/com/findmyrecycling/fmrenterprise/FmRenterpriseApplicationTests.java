711eb5fe36cfdffdcd20e03df02a62592b7e70b5
Gabriel Phan <phanga@mail.uc.edu>
2022-09-25 16:39:31 -0400

    Adding initial test for finding facility (Blue Ash) by ID (zipcode 45236)

--- src/test/java/com/findmyrecycling/fmrenterprise/FmRenterpriseApplicationTests.java
+++ src/test/java/com/findmyrecycling/fmrenterprise/FmRenterpriseApplicationTests.java
@@ -6,8 +6,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FmRenterpriseApplicationTests {
 
+
     @Test
     void contextLoads() {
     }
+    @Test
+    void fetchLocationByID_returns45236AsID()
+    {
+        givenFacilityDataIsAvailable();
+        whenSearchLocationWithID45236();
+        thenReturnListOfFacilitiesFor45236();
 
+    }
+
+    private void givenFacilityDataIsAvailable() {
+    }
+
+    private void whenSearchLocationWithID45236() {
+    }
+
+    private void thenReturnListOfFacilitiesFor45236() {
+    }
 }
