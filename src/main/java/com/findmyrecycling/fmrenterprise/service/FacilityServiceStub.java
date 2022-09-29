cad40d0e26c63854ffcfac651bd15e584e7fca33
Saleem Dweik <54520939+dweiksz@users.noreply.github.com>
2022-09-29 18:33:39 -0400

    Removed deprecated code
    IntelliJ was letting me know the use of new(Long (n)) was deprecated, so I updated it in order to address technical debt. This relates to a section within the article Clean Coding in Java where it says "Maintainable Codebase: Any software that we develop has a productive life and during this period will require changes and general maintenance. Clean code can help develop software that is easy to change and maintain over time" (Chandrakant). This relates to the project as removing deprecated features helps the code stay maintained.

--- src/main/java/com/findmyrecycling/fmrenterprise/service/FacilityServiceStub.java
+++ src/main/java/com/findmyrecycling/fmrenterprise/service/FacilityServiceStub.java
@@ -35,9 +35,9 @@ public class FacilityServiceStub implements IFacilityService{
     @java.lang.Override
     public List<Facility> fetchByAddress(String address) {
         List<Facility> facilities = new List<Facility>();
-        Facility facility = new Facility( new Long(1), new Long(1), "Facility", "Photo", "4 Carpenters Run, Blue Ash, OH 45241", new Decimal());
-        Facility facility2 = new Facility( new Long(2), new Long(2), "Facility2", "Photo", "6905 Plainfield Rd, Silverton, OH 45236", new Decimal());
-        Facility facility3 = new Facility( new Long(3), new Long(3), "Facility3", "Photo", "8041 Hosbrook Rd # 115, Cincinnati, OH 45236", new Decimal());
+        Facility facility = new Facility(1L, 1L, "Facility", "Photo", "4 Carpenters Run, Blue Ash, OH 45241", new Decimal());
+        Facility facility2 = new Facility(2L, 2L, "Facility2", "Photo", "6905 Plainfield Rd, Silverton, OH 45236", new Decimal());
+        Facility facility3 = new Facility( 3L, 3L, "Facility3", "Photo", "8041 Hosbrook Rd # 115, Cincinnati, OH 45236", new Decimal());
         facilities.add(facility);
         facilities.add(facility2);
         facilities.add(facility3);
