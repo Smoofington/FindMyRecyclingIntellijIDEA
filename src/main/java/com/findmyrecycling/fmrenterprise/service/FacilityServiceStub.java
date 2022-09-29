0cf0cc37709b44b4d885e55383c4bb16fd1b02de
Saleem Dweik <54520939+dweiksz@users.noreply.github.com>
2022-09-29 19:00:17 -0400

    Resolved build errors
    This commit was mostly to resolve build errors I was getting when trying to build your project. In Java, List is abstract so you cannot instantiate it in the way attempted. I also added the @Nullable annotation to FacilityServiceStub as it is recommended by the article "A short summary of Java coding best practices". It says "Encourage use of @Nullable when a field or method returns null" which it did (Hamedy).

--- src/main/java/com/findmyrecycling/fmrenterprise/service/FacilityServiceStub.java
+++ src/main/java/com/findmyrecycling/fmrenterprise/service/FacilityServiceStub.java
@@ -1,12 +1,15 @@
 package com.findmyrecycling.fmrenterprise.service;
-
 import com.findmyrecycling.fmrenterprise.Decimal;
 import com.findmyrecycling.fmrenterprise.dto.Facility;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class FacilityServiceStub implements IFacilityService{
     @java.lang.Override
+    @Nullable
     public List<Facility> fetchAll() {
         return null;
     }
@@ -34,7 +37,8 @@ public class FacilityServiceStub implements IFacilityService{
 
     @java.lang.Override
     public List<Facility> fetchByAddress(String address) {
-        List<Facility> facilities = new List<Facility>();
+        List<Facility> facilities;
+        facilities = new ArrayList<Facility>();
         Facility facility = new Facility(1L, 1L, "Facility", "Photo", "4 Carpenters Run, Blue Ash, OH 45241", new Decimal());
         Facility facility2 = new Facility(2L, 2L, "Facility2", "Photo", "6905 Plainfield Rd, Silverton, OH 45236", new Decimal());
         Facility facility3 = new Facility( 3L, 3L, "Facility3", "Photo", "8041 Hosbrook Rd # 115, Cincinnati, OH 45236", new Decimal());
