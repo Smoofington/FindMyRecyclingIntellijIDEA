687700c85a6e4691217041790d5aad7f12ba23f4
Christian Grothaus <cgrothaus@outlook.com>
2022-09-30 13:35:55 -0400

    Fix errors related to facility list and deprecated long instantiation

--- src/main/java/com/findmyrecycling/fmrenterprise/service/FacilityServiceStub.java
+++ src/main/java/com/findmyrecycling/fmrenterprise/service/FacilityServiceStub.java
@@ -4,6 +4,9 @@ import com.findmyrecycling.fmrenterprise.Decimal;
 import com.findmyrecycling.fmrenterprise.dto.Facility;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class FacilityServiceStub implements IFacilityService{
     @java.lang.Override
@@ -34,10 +37,10 @@ public class FacilityServiceStub implements IFacilityService{
 
     @java.lang.Override
     public List<Facility> fetchByAddress(String address) {
-        List<Facility> facilities = new List<Facility>();
-        Facility facility = new Facility( new Long(1), new Long(1), "Facility", "Photo", "4 Carpenters Run, Blue Ash, OH 45241", new Decimal());
-        Facility facility2 = new Facility( new Long(2), new Long(2), "Facility2", "Photo", "6905 Plainfield Rd, Silverton, OH 45236", new Decimal());
-        Facility facility3 = new Facility( new Long(3), new Long(3), "Facility3", "Photo", "8041 Hosbrook Rd # 115, Cincinnati, OH 45236", new Decimal());
+        List<Facility> facilities = new ArrayList<>();
+        Facility facility = new Facility(1L, 1L, "Facility", "Photo", "4 Carpenters Run, Blue Ash, OH 45241", new Decimal());
+        Facility facility2 = new Facility(2L, 2L, "Facility2", "Photo", "6905 Plainfield Rd, Silverton, OH 45236", new Decimal());
+        Facility facility3 = new Facility(3L, 3L, "Facility3", "Photo", "8041 Hosbrook Rd # 115, Cincinnati, OH 45236", new Decimal());
         facilities.add(facility);
         facilities.add(facility2);
         facilities.add(facility3);
