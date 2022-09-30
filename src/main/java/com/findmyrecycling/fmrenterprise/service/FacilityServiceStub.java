c29de65e6880ddd4bccc25c799415277d421d059
Christian Grothaus <cgrothaus@outlook.com>
2022-09-30 14:03:16 -0400

    Add fetchByParam() to IFacilityService and override it in Stub and Service. Refactor the test list of facilities in FacilityServiceStub to be a method to be reusable.

--- src/main/java/com/findmyrecycling/fmrenterprise/service/FacilityServiceStub.java
+++ src/main/java/com/findmyrecycling/fmrenterprise/service/FacilityServiceStub.java
@@ -9,6 +9,17 @@ import java.util.List;
 
 @Component
 public class FacilityServiceStub implements IFacilityService{
+
+    private List<Facility> createTestFacilities() {
+        List<Facility> facilities = new ArrayList<>();
+        Facility facility = new Facility(1L, 1L, "Facility", "Photo", "4 Carpenters Run, Blue Ash, OH 45241", new Decimal());
+        Facility facility2 = new Facility(2L, 2L, "Facility2", "Photo", "6905 Plainfield Rd, Silverton, OH 45236", new Decimal());
+        Facility facility3 = new Facility(3L, 3L, "Facility3", "Photo", "8041 Hosbrook Rd # 115, Cincinnati, OH 45236", new Decimal());
+        facilities.add(facility);
+        facilities.add(facility2);
+        facilities.add(facility3);
+        return facilities;
+    }
     @java.lang.Override
     public List<Facility> fetchAll() {
         return null;
@@ -37,14 +48,11 @@ public class FacilityServiceStub implements IFacilityService{
 
     @java.lang.Override
     public List<Facility> fetchByAddress(String address) {
-        List<Facility> facilities = new ArrayList<>();
-        Facility facility = new Facility(1L, 1L, "Facility", "Photo", "4 Carpenters Run, Blue Ash, OH 45241", new Decimal());
-        Facility facility2 = new Facility(2L, 2L, "Facility2", "Photo", "6905 Plainfield Rd, Silverton, OH 45236", new Decimal());
-        Facility facility3 = new Facility(3L, 3L, "Facility3", "Photo", "8041 Hosbrook Rd # 115, Cincinnati, OH 45236", new Decimal());
-        facilities.add(facility);
-        facilities.add(facility2);
-        facilities.add(facility3);
-        return facilities;
+        return createTestFacilities();
     }
 
+    @Override
+    public List<Facility> fetchByParam(String facilityName, String facilityDetails, String facilityAddress, String facilityCoordinates) {
+        return createTestFacilities();
+    }
 }
--- src/main/java/com/findmyrecycling/fmrenterprise/service/IFacilityService.java
+++ src/main/java/com/findmyrecycling/fmrenterprise/service/IFacilityService.java
@@ -1,6 +1,7 @@
 package com.findmyrecycling.fmrenterprise.service;
 
 import com.findmyrecycling.fmrenterprise.dto.Facility;
+import org.springframework.lang.Nullable;
 
 import java.util.List;
 
@@ -10,23 +11,27 @@ public interface IFacilityService {
      * @return list of facilities or null if none
      */
     List<Facility> fetchAll();
+
     /**
      * Fetch all facilities using an ID
      * @param id a unique identifier for a facility
      * @return list of facilities or null if none
      */
     Facility fetchById(int id);
+
     /**
      * Show location of facility on map
      * @param facility is a facility containing a lat and long
      */
     void sendToMaps(Facility facility);
+
     /**
      * Save facility
      * @param facility is a Facility object
      * @return the facility that was saved
      */
     Facility save(Facility facility);
+
     /**
      * Delete the record of the facility
      * @param id is a unique identifier for the facility
@@ -41,4 +46,13 @@ public interface IFacilityService {
      */
     List<Facility> fetchByAddress(String address);
 
+    /**
+     * Fetch the facilities by given params.
+     * @param facilityName Optional param that is the name of the facility
+     * @param facilityDetails Optional param that is the details linked to the facility
+     * @param facilityAddress Optional param that is the address of the facility
+     * @param facilityCoordinates Optional param that is the coordinates of the facility
+     * @return a list of facilities that match the params
+     */
+    List<Facility> fetchByParam( @Nullable String facilityName, @Nullable String facilityDetails, @Nullable String facilityAddress, @Nullable String facilityCoordinates);
 }
