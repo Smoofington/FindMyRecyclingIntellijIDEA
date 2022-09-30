5dfbdee6b9c773bd8b5347a89d3b733098180259
Christian Grothaus <cgrothaus@outlook.com>
2022-09-30 18:19:33 -0400

    Import Java List to Fix Error

--- src/test/java/com/findmyrecycling/fmrenterprise/FmRenterpriseApplicationTests.java
+++ src/test/java/com/findmyrecycling/fmrenterprise/FmRenterpriseApplicationTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
