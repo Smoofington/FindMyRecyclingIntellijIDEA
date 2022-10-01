be10a0f76b5bbc637ba7a594ca53468c76dc16eb
Christian Grothaus <cgrothaus@outlook.com>
2022-09-30 22:25:40 -0400

    Update DTO classes to use Lombok @Data

--- src/main/java/com/findmyrecycling/fmrenterprise/dto/Facility.java
+++ src/main/java/com/findmyrecycling/fmrenterprise/dto/Facility.java
@@ -1,6 +1,9 @@
 package com.findmyrecycling.fmrenterprise.dto;
 
-public class Facility {
+import lombok.Data;
+
+public @Data
+class Facility {
     private Long facilityId;
     private Long materialId;
     private String facilityName;
@@ -12,46 +15,6 @@ public class Facility {
         this.materialId = materialId;
         this.facilityName = facilityName;
         this.facilityPhotos = facilityPhotos;
-        this.facilityAddress = facilityAddress;
-    }
-
-    public Long getFacilityId() {
-        return facilityId;
-    }
-
-    public Long getMaterialId() {
-        return materialId;
-    }
-
-    public String getFacilityName() {
-        return facilityName;
-    }
-
-    public String getFacilityPhotos() {
-        return facilityPhotos;
-    }
-
-    public String getFacilityAddress() {
-        return facilityAddress;
-    }
-
-    public void setFacilityId(Long facilityId) {
-        this.facilityId = facilityId;
-    }
-
-    public void setMaterialId(Long materialId) {
-        this.materialId = materialId;
-    }
-
-    public void setFacilityName(String facilityName) {
-        this.facilityName = facilityName;
-    }
-
-    public void setFacilityPhotos(String facilityPhotos) {
-        this.facilityPhotos = facilityPhotos;
-    }
-
-    public void setFacilityAddress(String facilityAddress) {
         this.facilityAddress = facilityAddress;
     }
 
