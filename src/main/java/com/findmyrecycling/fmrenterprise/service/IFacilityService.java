c91abaadb81bc2eca73609f68e0968aa1060c11c
momoney13 <64868507+momoney13@users.noreply.github.com>
2022-10-02 17:22:54 -0400

    Edited some whitespace for readability

--- src/main/java/com/findmyrecycling/fmrenterprise/service/IFacilityService.java
+++ src/main/java/com/findmyrecycling/fmrenterprise/service/IFacilityService.java
@@ -2,7 +2,6 @@ package com.findmyrecycling.fmrenterprise.service;
 
 import com.findmyrecycling.fmrenterprise.dto.Facility;
 import org.springframework.lang.Nullable;
-
 import java.util.List;
 
 public interface IFacilityService {
@@ -11,27 +10,23 @@ public interface IFacilityService {
      * @return list of facilities or null if none
      */
     List<Facility> fetchAll();
-
     /**
      * Fetch all facilities using an ID
      * @param id a unique identifier for a facility
      * @return list of facilities or null if none
      */
     Facility fetchById(int id);
-
     /**
      * Save facility
      * @param facility is a Facility object
      * @return the facility that was saved
      */
     Facility save(Facility facility);
-
     /**
      * Delete the record of the facility
      * @param id is a unique identifier for the facility
      */
     void delete(int id);
-
     /**
      * Fetch the facilities by given params.
      * @param term String to check against all params of facilities
