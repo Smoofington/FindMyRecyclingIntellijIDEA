4449eb16d51f826a05981a4445b0d1377bf6e006
alphapixel1 <alphapixel2@gmail.com>
2022-10-02 03:01:22 -0400

    commented, changed html and css

--- src/main/java/com/findmyrecycling/fmrenterprise/FindMyRecyclingController.java
+++ src/main/java/com/findmyrecycling/fmrenterprise/FindMyRecyclingController.java
@@ -16,26 +16,48 @@ public class FindMyRecyclingController {
     @Autowired
     IFacilityService facilityService;
 
+    /**
+     * index page
+     * @return
+     */
     @RequestMapping("/")
     public String index() {
         return "index";
     }
 
+    /**
+     *
+     * @return facility page
+     */
     @RequestMapping("/AddFacility.html")
     public String addFacilityPage() {
         return "AddFacility";
     }
 
+    /**
+     * Get all facilities
+     * @return all facilities and a 200 code
+     */
     @GetMapping("/facility/")
     public ResponseEntity fetchAllFacilities() {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    /**
+     *
+     * @param id
+     * @return returns the facility with the id 'id'
+     */
     @GetMapping("/facility/{id}/")
     public ResponseEntity fetchFacilityById(@PathVariable("id") int id) {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    /**
+     *
+     * @param term term to search for facilities by
+     * @return list of facilities that use the term
+     */
     @GetMapping(value = "/facility/search/{term}/", produces = "application/json")
     public List<Facility> fetchFacilitiesByTerm(@PathVariable("term") String term
     ) {
@@ -43,17 +65,33 @@ public class FindMyRecyclingController {
         return facilities;
     }
 
+    /**
+     * Deletes the facility with that id
+     * @param id id of facility to delete
+     * @return 200 if id Exists
+     */
     @DeleteMapping("/facility/{id}/")
     public ResponseEntity deleteFacilityById(@PathVariable("id") int id) {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    /**
+     * Updates the facility
+     * @param facility new facility
+     * @param id id of the facility to update
+     * @return 200 if the facility exists
+     */
     @PatchMapping("/facility/{id}/")
     public ResponseEntity updateFacilityById(@RequestBody Facility facility, @PathVariable("id") int id
     ) {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    /**
+     * Creates the facility
+     * @param facility
+     * @return returns 200 http status
+     */
     @PutMapping("/facility/")
     public ResponseEntity createFacility(@RequestBody Facility facility
     ) {
