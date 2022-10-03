831b297169ca5b6bb8220fab548fef1f92745e2c
abbottmh <abbottmh@mail.uc.edu>
2022-10-02 20:08:25 -0400

    CSS and placeholder comment tags
    Changed a couple lines of css to simplify it and fix an error. Also added some placeholder comment tags to the FindMyRecyclingController.java just to point out that you should add javadoc here.

--- src/main/java/com/findmyrecycling/fmrenterprise/FindMyRecyclingController.java
+++ src/main/java/com/findmyrecycling/fmrenterprise/FindMyRecyclingController.java
@@ -16,44 +16,45 @@ public class FindMyRecyclingController {
     @Autowired
     IFacilityService facilityService;
 
+    /* */
     @RequestMapping("/")
     public String index() {
         return "index";
     }
-
+     /* */
     @RequestMapping("/AddFacility.html")
     public String addFacilityPage() {
         return "AddFacility";
     }
-
+     /* */
     @GetMapping("/facility/")
     public ResponseEntity fetchAllFacilities() {
         return new ResponseEntity(HttpStatus.OK);
     }
-
+     /* */
     @GetMapping("/facility/{id}/")
     public ResponseEntity fetchFacilityById(@PathVariable("id") int id) {
         return new ResponseEntity(HttpStatus.OK);
     }
-
+     /* */
     @GetMapping(value = "/facility/search/{term}/", produces = "application/json")
     public List<Facility> fetchFacilitiesByTerm(@PathVariable("term") String term
     ) {
         List<Facility> facilities = facilityService.fetchByGlobalSearch(term);
         return facilities;
     }
-
+     /* */
     @DeleteMapping("/facility/{id}/")
     public ResponseEntity deleteFacilityById(@PathVariable("id") int id) {
         return new ResponseEntity(HttpStatus.OK);
     }
-
+     /* */
     @PatchMapping("/facility/{id}/")
     public ResponseEntity updateFacilityById(@RequestBody Facility facility, @PathVariable("id") int id
     ) {
         return new ResponseEntity(HttpStatus.OK);
     }
-
+     /* */
     @PutMapping("/facility/")
     public ResponseEntity createFacility(@RequestBody Facility facility
     ) {
