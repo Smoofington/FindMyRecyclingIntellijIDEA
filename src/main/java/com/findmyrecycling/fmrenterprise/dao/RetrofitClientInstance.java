32383f1943272b37e382e23eb6e8061cb9a06c62
Gabriel Phan <phanga@mail.uc.edu>
2022-10-28 18:13:47 -0400

    fixing url for JSON data

--- src/main/java/com/findmyrecycling/fmrenterprise/dao/RetrofitClientInstance.java
+++ src/main/java/com/findmyrecycling/fmrenterprise/dao/RetrofitClientInstance.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClientInstance {
 
     private static Retrofit retrofit;
-    private static String BASE_URL = "FMRJSONdata.md";
+    private static String BASE_URL = "https://github.com/";
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
