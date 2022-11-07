bc5b3e18b2d896f44df00758862b08b613e05e9e
samgehrlich <gehrlism@mail.uc.edu>
2022-11-07 10:05:24 -0500

    reformatted the base_url

--- src/main/java/com/findmyrecycling/fmrenterprise/dao/RetrofitClientInstance.java
+++ src/main/java/com/findmyrecycling/fmrenterprise/dao/RetrofitClientInstance.java
@@ -6,10 +6,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClientInstance {
 
     private static Retrofit retrofit;
-    private static String BASE_URL = "";
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
+            String BASE_URL = "";
             retrofit = new retrofit2.Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
