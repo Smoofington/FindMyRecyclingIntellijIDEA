476714b928ac029d47fddb8ca6687570fddcec9d
Capy <cgrothaus@outlook.com>
2022-11-12 11:21:32 -0500

    Updated the FacilityDAO interface to take correct types for ids and remove @Nullable which was unnecessary

--- src/main/java/com/findmyrecycling/fmrenterprise/dao/IFacilityDAO.java
+++ src/main/java/com/findmyrecycling/fmrenterprise/dao/IFacilityDAO.java
@@ -1,20 +1,14 @@
 package com.findmyrecycling.fmrenterprise.dao;
 
 import com.findmyrecycling.fmrenterprise.dto.Facility;
-import org.springframework.lang.Nullable;
 
 import java.io.IOException;
 import java.util.List;
 
 public interface IFacilityDAO {
     Facility save(Facility facility);
-
     List<Facility> fetchAll();
-
-    @Nullable
-    Facility fetchById(int id);
-
-    void delete(int id);
-
+    Facility fetchById(long id);
+    void delete(long id);
     List<Facility> fetchByGlobalSearch(String term) throws IOException;
 }
