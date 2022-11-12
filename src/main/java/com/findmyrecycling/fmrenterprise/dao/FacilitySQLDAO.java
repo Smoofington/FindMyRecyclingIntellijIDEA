a188bfeee714728f1dff473ab61ee6b7cb4ebcbc
Capy <cgrothaus@outlook.com>
2022-11-12 11:24:02 -0500

    Created SQL DAO for Photo, Material, and Facility.

---
+++ src/main/java/com/findmyrecycling/fmrenterprise/dao/FacilitySQLDAO.java
@@ -0,0 +1,56 @@
+package com.findmyrecycling.fmrenterprise.dao;
+
+import com.findmyrecycling.fmrenterprise.dto.Facility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository("facilitySQLDAO")
+public class FacilitySQLDAO implements IFacilityDAO{
+
+    @Autowired
+    FacilityRepository facilityRepository;
+
+    @Override
+    public Facility save(Facility facility) {
+        Facility createdFacility = facilityRepository.save(facility);
+        return createdFacility;
+    }
+
+    @Override
+    public List<Facility> fetchAll() {
+        List<Facility> allFacilitiesList = new ArrayList<>();
+        Iterable<Facility> allFacilities = facilityRepository.findAll();
+        for(Facility facility : allFacilities) {
+            allFacilitiesList.add(facility);
+        }
+        return allFacilitiesList;
+    }
+
+    @Override
+    public Facility fetchById(long id) {
+        Optional<Facility> retrievedFacility = facilityRepository.findById(id);
+        return retrievedFacility.orElse(null);
+    }
+
+    @Override
+    public void delete(long id) {
+        facilityRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Facility> fetchByGlobalSearch(String term) throws IOException {
+        List<Facility> matchingFacilities = new ArrayList<>();
+        Iterable<Facility> allFacilities = facilityRepository.findAll();
+        for(Facility facility : allFacilities) {
+            if (facility.toString().contains(term)) {
+                matchingFacilities.add(facility);
+            }
+        }
+        return matchingFacilities;
+    }
+}
