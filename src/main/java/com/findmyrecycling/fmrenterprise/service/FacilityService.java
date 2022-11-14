d320cffb62675473a5ebf9aede275ba6cec2e8f9
Gabriel Phan <phanga@mail.uc.edu>
2022-11-13 19:40:29 -0500

    Having the cache over read operations

--- src/main/java/com/findmyrecycling/fmrenterprise/service/FacilityService.java
+++ src/main/java/com/findmyrecycling/fmrenterprise/service/FacilityService.java
@@ -3,6 +3,7 @@ package com.findmyrecycling.fmrenterprise.service;
 import com.findmyrecycling.fmrenterprise.dao.IFacilityDAO;
 import com.findmyrecycling.fmrenterprise.dto.Facility;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -23,11 +24,13 @@ public class FacilityService implements IFacilityService{
     }
 
     @Override
+    @Cacheable("facilities")
     public List<Facility> fetchAll() {
         return facilityDAO.fetchAll();
     }
 
     @Override
+    @Cacheable("facility")
     public Facility fetchById(int id) {
         return facilityDAO.fetchById(id);
     }
@@ -43,6 +46,7 @@ public class FacilityService implements IFacilityService{
     }
 
     @Override
+    @Cacheable("areas")
     public List<Facility> fetchByGlobalSearch(String term) throws IOException {
         return facilityDAO.fetchByGlobalSearch(term);
     }
